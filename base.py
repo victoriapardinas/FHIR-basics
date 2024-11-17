599609a8bdb1d21859f89a13c63ef3aab43fd617
victoriapardinas <vpardinas@itba.edu.ar>
2024-11-17 00:10:59 +0000

    Base original

--- base.py
+++ base.py
@@ -1,26 +1,33 @@
 
 import requests
+from patient import create_patient_resource
 
-# Buscar paciente por documento (identifier)
-def search_patient_by_document(document_number):
-    url = f"http://hapi.fhir.org/baseR4/Patient?identifier={document_number}"
+# Enviar el recurso FHIR al servidor HAPI FHIR
+def send_resource_to_hapi_fhir(resource, resource_type):
+    url = f"http://hapi.fhir.org/baseR4/{resource_type}"
+    headers = {"Content-Type": "application/fhir+json"}
+    resource_json = resource.json()
+
+    response = requests.post(url, headers=headers, data=resource_json)
+
+    if response.status_code == 201:
+        print("Recurso creado exitosamente")
+        
+        # Devolver el ID del recurso creado
+        return response.json()['id']
+    else:
+        print(f"Error al crear el recurso: {response.status_code}")
+        print(response.json())
+        return None
+
+# Buscar el recurso por ID
+def get_resource_from_hapi_fhir(resource_id, resource_type):
+    url = f"http://hapi.fhir.org/baseR4/{resource_type}/{resource_id}"
     response = requests.get(url, headers={"Accept": "application/fhir+json"})
 
     if response.status_code == 200:
-        # Si la búsqueda es exitosa, devuelve los resultados
-        patients = response.json().get('entry', [])
-        
-        if patients:
-            print(f"Paciente(s) encontrado(s) con el documento {document_number}:")
-            for patient_entry in patients:
-                patient = patient_entry['resource']
-                print(f"ID: {patient['id']}")
-                print(f"Nombre: {patient['name'][0]['given'][0]} {patient['name'][0]['family']}")
-                print(f"Fecha de nacimiento: {patient['birthDate']}")
-                print(f"Género: {patient['gender']}")
-                print("-------------------------------")
-        else:
-            print(f"No se encontró ningún paciente con el documento {document_number}.")
+        resource = response.json()
+        print(resource)
     else:
-        print(f"Error al buscar el paciente: {response.status_code}")
+        print(f"Error al obtener el recurso: {response.status_code}")
         print(response.json())
