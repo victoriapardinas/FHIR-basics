2ed53315bd8173f62773efad62edd7f30c4e741b
victoriapardinas <vpardinas@itba.edu.ar>
2024-11-18 03:06:10 +0000

    Paciente con identificador + prueba con correcciones

--- patient_with_id.py
+++ patient_with_id.py
@@ -3,11 +3,11 @@ from fhir.resources.patient import Patient
 from fhir.resources.humanname import HumanName
 from fhir.resources.contactpoint import ContactPoint
 
-# Crear el recurso FHIR de paciente con parámetros opcionales
+
 def create_patient_resource(family_name=None, given_name=None, birth_date=None, gender=None, phone=None, document_number=None):
     patient = Patient()
     
-    # Agregar el nombre del paciente si está disponible
+    
     if family_name or given_name:
         name = HumanName()
         if family_name:
@@ -16,15 +16,15 @@ def create_patient_resource(family_name=None, given_name=None, birth_date=None, 
             name.given = [given_name]
         patient.name = [name]
     
-    # Agregar la fecha de nacimiento si está disponible
+    
     if birth_date:
         patient.birthDate = birth_date
 
-    # Agregar el género si está disponible
+    
     if gender:
         patient.gender = gender
 
-    # Agregar información de contacto si está disponible
+    
     if phone:
         contact = ContactPoint()
         contact.system = "phone"
@@ -32,7 +32,7 @@ def create_patient_resource(family_name=None, given_name=None, birth_date=None, 
         contact.use = "mobile"
         patient.telecom = [contact]
     
-    # Agregar el identificador (número de documento) si está disponible
+    
     if document_number:
         patient.identifier = [{
             "use": "official",  # Identificador oficial
--- prueba_paciente.py
+++ prueba_paciente.py
@@ -2,23 +2,22 @@
 from patient_with_id import create_patient_resource
 from base import send_resource_to_hapi_fhir, get_resource_from_hapi_fhir
 
-# Parámetros del paciente (usando los datos de Milhouse Martinez)
 family_name = "Martinez"
 given_name = "Milhouse"
 birth_date = "1993-09-10"
 gender = "male"
-phone = "+1123456789"  # Número de teléfono de ejemplo
-document_number = "35783587"  # Número de documento
+phone = "+1123456789"  
+document_number = "35783587"  
 
-# Crear el recurso de paciente con identificador
+
 patient_with_id = create_patient_resource(family_name, given_name, birth_date, gender, phone, document_number)
 
 patient_id = send_resource_to_hapi_fhir(patient_with_id, 'Patient')
 
-# Si el paciente fue creado exitosamente, obtener el recurso del paciente
+
 if patient_id:
     print(f"Paciente con ID {patient_id} creado exitosamente.")
-    # Ahora leer el recurso de paciente usando el ID
+
     get_resource_from_hapi_fhir(patient_id, 'Patient')
 else:
     print("Error al crear el paciente.")
