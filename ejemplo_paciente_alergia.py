2fe79aea8e9a18e01e2f712855edbd083904f3f6
victoriapardinas <vpardinas@itba.edu.ar>
2024-11-20 00:30:20 +0000

    Ejemplo de uso recurso de Alergias

---
+++ ejemplo_paciente_alergia.py
@@ -0,0 +1,3 @@
+
+patient_id = "45179080"  # ID del paciente
+get_allergies_by_patient_id(patient_id)
--- obtener_alergia_paciente.py
+++ obtener_alergia_paciente.py
@@ -4,15 +4,15 @@ import requests
 def get_allergies_by_patient_id(patient_id):
     url = f"http://hapi.fhir.org/baseR4/AllergyIntolerance?patient={patient_id}"
     response = requests.get(url, headers={"Accept": "application/fhir+json"})
-    
+
     if response.status_code == 200:
         allergies = response.json().get('entry', [])
-        
+
         if allergies:
             print(f"Alergias encontradas para el paciente con ID {patient_id}:")
             for allergy_entry in allergies:
                 allergy = allergy_entry['resource']
-                
+
                 print(f"ID de la alergia: {allergy.get('id', 'No ID disponible')}")
 
                 if 'reaction' in allergy and len(allergy['reaction']) > 0:
@@ -37,7 +37,7 @@ def get_allergies_by_patient_id(patient_id):
                         print("Reacción: No disponible")
                 else:
                     print("Severidad o Reacción: No disponible")
-                
+
                 category = allergy.get('category', 'No disponible')
                 print(f"Categoría: {category}")
 
@@ -51,7 +51,7 @@ def get_allergies_by_patient_id(patient_id):
                         print("Nota: No disponible")
                 else:
                     print("Nota: No disponible")
-                
+
                 print("-------------------------------")
         else:
             print(f"No se encontraron alergias para el paciente con ID {patient_id}.")
